f6e92809aa78bf38b98ba0a694a3136115a440ca
Jane Marimo <jenny.bulatova@gmail.com>
2023-10-03 12:21:16 +0300

    preloader done

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,43 @@
+const itemsWrapper = document.querySelector('#items');
+const url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function(event) {
+    if(this.readyState === 4 && this.status === 200) {
+        let responseText = this.responseText;
+        let parsedResponse = JSON.parse.parse(responseText);
+        let valuteObj = parsedResponse.response.Valute;
+
+        for(const currencyCode in valuteObj) {
+            if(valuteObj.hasOwnProperty(currencyCode)) {
+                const currencyItem = valuteObj[currencyCode];
+
+                let divItemCode = document.createElement('div');
+                divItemCode.className = 'item__code';
+                divItemCode.innerHTML = currencyItem.CharCode;
+
+                let divItemValue = duocument.createElement('div');
+                divItemValue.className = 'item__value';
+                divItemValue.innerHTML = currencyItem.Value;
+
+                let divItemCurrency = document.createElement('div');
+                divItemCurrency.className = 'item__currency';
+                divItemCurrency.innerHTML = 'руб.';
+
+                let itemsContainer = document.createElement('div');
+                itemsContainer.classNmae - 'item';
+                
+                itemsContainer.appendChild(divItemCode);
+                itemsContainer.appendChild(divItemValue);
+                itemsContainer.appendChild(divItemCurrency);
+                itemsWrapper.appendChild(itemsContainer);
+
+            };
+        };
+        loader.classList.toggle('loader__active');
+    };
+};
+
+xhr.send();
